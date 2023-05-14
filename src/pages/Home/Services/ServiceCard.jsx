e283715c18daa8f3bb2805fd16687f7c2ee517ce
Atikur <atikurrahman7223@gmail.com>
2023-05-14 20:28:55 +0600

    added services section

---
+++ src/pages/Home/Services/ServiceCard.jsx
@@ -0,0 +1,22 @@
+import { FaArrowRight } from "react-icons/fa";
+
+const ServiceCard = ({ service }) => {
+  const { img, title, price } = service;
+
+  return (
+    <div className="card card-compact rounded-lg border border-inherit p-6">
+      <figure className="rounded-none">
+        <img src={img} alt="service" className="h-52 w-full rounded-lg" />
+      </figure>
+      <div className="space-y-5">
+        <h2 className="card-title text-2xl font-bold pt-5">{title}</h2>
+        <div className="inline-flex items-center text-xl font-semibold text-[#FF3811]">
+          <p>Price: ${price}</p>
+          <span> <FaArrowRight /> </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ServiceCard;
--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -1,11 +1,30 @@
+import { useEffect, useState } from "react";
+import ServiceCard from "./ServiceCard";
 
 const Services = () => {
+    const [services, setServices] = useState([]);
+
+    useEffect( () => {
+        fetch('services.json')
+         .then(res => res.json())
+         .then(data => setServices(data))
+    }, [])
     return (
+        <>
         <div className="text-center space-y-5">
-            <h5 className='text-xl font-semibold md:font-bold text-[#FF3811] pt-16'>About Us</h5>
+            <h5 className='text-xl font-semibold md:font-bold text-[#FF3811] pt-16'>Service</h5>
             <h2 className='text-3xl md:text-5xl font-semibold md:font-bold md:tracking-wide'>Our Service Area</h2>
-            <p className='text-gray-500 md:leading-7'>The majority have suffered alteration in some form, by injected humour, or randomised <br /> words which do not look even slightly believable.</p>
+            <p className='text-gray-500 md:leading-7 pb-12'>The majority have suffered alteration in some form, by injected humour, or randomised <br /> words which do not look even slightly believable.</p>
         </div>
+        <div className="my-margin-zero grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {
+                services?.map( service => <ServiceCard key={service._id} service={service} />)
+            }
+        </div>
+        <div className="text-center my-margin-zero pt-12">
+            <button className="my-outline-btn">See More</button>
+        </div>
+        </>
     );
 };
 
