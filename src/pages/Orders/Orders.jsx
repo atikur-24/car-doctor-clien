6f99fc715ce64c5189412b5fe459bd71157a7d11
Atikur <atikurrahman7223@gmail.com>
2023-05-17 14:43:20 +0600

    service status update or delete

--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -6,13 +6,14 @@ import OrdersRow from "./OrdersRow";
 const Orders = () => {
     const { user } = useContext(AuthContext);
     const [orders, setOrders] = useState([]);
+    const [controls, setControls] = useState(false);
 
     const url = `http://localhost:5000/orders?email=${user?.email}`
     useEffect( () => {
         fetch(url)
          .then(res => res.json())
          .then(data => setOrders(data))
-    }, [])
+    }, [ controls ])
 
     return (
         <section>
@@ -23,7 +24,7 @@ const Orders = () => {
                 <table className="table w-full">
                     <tbody>
                         {
-                            orders.map( order => <OrdersRow key={order._id} order={order} />)
+                            orders.map( order => <OrdersRow key={order._id} order={order} controls={controls} setControl={setControls} />)
                         }
                     </tbody>
                 </table>
--- src/pages/Orders/OrdersRow.jsx
+++ src/pages/Orders/OrdersRow.jsx
@@ -1,11 +1,66 @@
+import Swal from "sweetalert2";
 
-const OrdersRow = ({ order }) => {
-    const { service, date, price, img } = order;
+const OrdersRow = ({ order, controls, setControl }) => {
+    const { _id, service, date, price, img, status } = order;
+    console.log(order);
+
+    const handleDelete = id => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+          }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/orders/${id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if(data.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                              )
+                              setControl(!controls);
+                        }
+                    })
+              
+            }
+          })
+        
+    }
+    
+    const handleOrderConfirm = id => {
+        fetch(`http://localhost:5000/orders/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ status : 'Confirm'})
+        })
+            .then(res => res.json())
+            .then(data => {
+                if(data.modifiedCount > 0) {
+                    Swal.fire({
+                        title: 'Confirmed!',
+                        text: 'Service confirm successfully!',
+                        icon: 'success',
+                        confirmButtonText: 'Ok'
+                      })
+                      setControl(!controls)
+                }
+            })
+    }
 
     return (
         <tr>
             <td>
-                <button className="btn btn-circle">
+                <button onClick={ () => handleDelete(_id) } className="btn btn-circle btn-sm">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
                 </button>
             </td>
@@ -24,7 +79,9 @@ const OrdersRow = ({ order }) => {
             <td className="text-xl font-semibold"> ${price} </td>
             <td className="text-xl font-medium">{date}</td>
             <td>
-                <button className="my-btn">Pending</button>
+                { status === "Confirm" ? <button className="my-btn-confirm">Confirmed</button> :
+                    <button onClick={ () => handleOrderConfirm(_id) } className="my-btn-pending">Pending</button>
+                }
             </td>
         </tr>
     );
